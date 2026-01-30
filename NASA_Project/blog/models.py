4d1e0bf1ce77e880d5b138a77d37004a12851b7f
William <ricewi@otc.edu>
2026-01-30 01:56:39 -0600

    Halfway through the chapter, page 21

---
+++ NASA_Project/blog/models.py
@@ -0,0 +1,64 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# I'm unsure what models I'll need yet
+
+class Post(models.Model):
+
+    # Defines possible statuses for ensuring unfinished posts aren't published
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    # Article title
+    article_title = models.CharField(max_length = 75)
+
+    # Update Post title
+    post_title = models.CharField(max_length = 75)
+
+    # Slug for url amendment to take users to specific post
+    slug = models.SlugField(max_length = 75)
+
+    # Allows Authors of posts to be recognized
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        # If author is deleted, posts stay, in documentation link on page 20 of book
+        on_delete=models.SET_NULL,
+        # Allows author value to be empty, in documentation link on page 20 of book
+        null=True,
+        # Allows form to accept empty values, in documentation link on page 20 of book
+        blank=True,
+        # Allows searching for authors posts
+        related_name = 'blog_posts'
+    )
+
+    # Content of post
+    body = models.TextField()
+
+    # Timestamp for post publication
+    publish = models.DateTimeField(default=timezone.now)
+
+    # Tracking of original creation date, for others use in reference materials
+    created = models.DateTimeField(auto_now_add = True)
+
+    # Data recency stamp so users know the last time the data was validated
+    updated = models.DateTimeField(auto_now = True)
+
+    # Limits value to those within the Status class, and assigns a default of draft requiring committal to post
+    status = models.CharField(
+        max_length = 2,
+        choices = Status,
+        default = Status.DRAFT
+    )
+    # for displaying posts on page in ascending order, oldest first
+    class Meta:
+        ordering = ['publish']
+        indexes = [
+            models.Index(fields = ['publish']),
+        ]
+
+
+    def __str__(self):
+        return self.title
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-2e0aaae1b6195c2367325f4f02e2d04e9abb55f0b24a779b69b11b9e10269abc.svg)](https://classroom.github.com/online_ide?assignment_repo_id=22440816&assignment_repo_type=AssignmentRepo)
 # CSC_161_Personal_Project
+
+# This is a working personal project that is NASA oriented, and will likely track upcoming missions, launch dates and progress updates.
+
+## It is constantly evolving and may change forms so update will be made here as necessary.
